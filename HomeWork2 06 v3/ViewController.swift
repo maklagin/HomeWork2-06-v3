a343bf044e985f7e599e79c63ba1a45e89089804
maklagin <108239343+maklagin@users.noreply.github.com>
2022-07-02 21:45:51 +0300

    Initial Commit

---
+++ HomeWork2 06 v3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork2 06 v3
+//
+//  Created by Максим Кулагин on 02.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
